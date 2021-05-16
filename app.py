429abd65182c56642019dcee6a226778ddd7d925
SejalJain-07 <sejal99jain.sj@gmail.com>
2021-05-16 11:35:38 +0530

    changes

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-from flask import Flask,flash, render_template, request
+from flask import Flask, render_template, request
 from tensorflow.keras.models import  load_model
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
+import re
+from nltk.corpus import stopwords
 
 app = Flask(__name__, template_folder='templates')
 
@@ -10,14 +12,36 @@ max_features=33000
 tokenizer = Tokenizer(num_words = max_features, filters='!"#$%&()*+,-./:;<=>?@[\\]^_`{|}~\t\n', lower = True, split = ' ')
 result=""
 lstm_prediction=None
+
+stop_words = set(stopwords.words('english'))
+to_remove = ['•', '!', '"', '#', '”', '“', '$', '%', '&', "'", '–', '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '…']
+stop_words.update(to_remove)
+
+"""def clean_text(text):
+    text = text.lower()
+    text = re.sub(r'http\S+', '', text)
+    text = re.sub('\[[^]]*\]', '', text)
+    text = (" ").join([word for word in text.split() if not word in stop_words])
+    text = "".join([char for char in text if not char in to_remove])
+    return text"""
+
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
 @app.route('/Prediction', methods=['GET', 'POST'])
 def prediction():
+    title = request.form['Name']
     text = request.form['Message']
-    news = str(text)
+    author = request.form['Subject']
+    total = title + '' + text + '' + author
+    news = str(total)
+    news = re.sub(r'http\S+', '', news)
+    news = re.sub('\[[^]]*\]', '', news)
+    news = (" ").join([word for word in news.split() if not word in stop_words])
+    news = "".join([char for char in news if not char in to_remove])
+    """news = news.apply(clean_text)"""
     tokenizer.fit_on_texts(texts = news)
     test_text = tokenizer.texts_to_sequences(texts = [news])
     test_text = pad_sequences(sequences = test_text, maxlen = max_features, padding = 'pre')
@@ -27,10 +51,6 @@ def prediction():
         result = "News is fake"
     else:
         result = "News is true"  
-    
-    """pred=sum(lstm_prediction)"""
-    
-    """pred=pred/2"""
     
     """for i in range(len(lstm_prediction)):
         if lstm_prediction[i].item() >= 0.5:
