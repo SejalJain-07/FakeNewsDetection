46dc7d7f959aa715221fcd8664556b63e14b9390
SejalJain-07 <sejal99jain.sj@gmail.com>
2021-05-16 12:55:14 +0530

    final changes and result page redirection

--- app.py
+++ app.py
@@ -7,23 +7,15 @@ from nltk.corpus import stopwords
 
 app = Flask(__name__, template_folder='templates')
 
-saved_covid_lstm = load_model('bidirectiona-lstm-model.h5')
+saved_covid_blstm = load_model('bidirectional-lstm-model.h5')
 max_features=33000
 tokenizer = Tokenizer(num_words = max_features, filters='!"#$%&()*+,-./:;<=>?@[\\]^_`{|}~\t\n', lower = True, split = ' ')
 result=""
-lstm_prediction=None
+blstm_prediction=None
 
 stop_words = set(stopwords.words('english'))
 to_remove = ['•', '!', '"', '#', '”', '“', '$', '%', '&', "'", '–', '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '…']
 stop_words.update(to_remove)
-
-"""def clean_text(text):
-    text = text.lower()
-    text = re.sub(r'http\S+', '', text)
-    text = re.sub('\[[^]]*\]', '', text)
-    text = (" ").join([word for word in text.split() if not word in stop_words])
-    text = "".join([char for char in text if not char in to_remove])
-    return text"""
 
 
 @app.route('/')
@@ -46,22 +38,12 @@ def prediction():
     test_text = tokenizer.texts_to_sequences(texts = [news])
     test_text = pad_sequences(sequences = test_text, maxlen = max_features, padding = 'pre')
     print(test_text) 
-    lstm_prediction = saved_covid_lstm.predict_classes(test_text)
-    if lstm_prediction == 1 :
+    blstm_prediction = saved_covid_blstm.predict_classes(test_text)
+    if blstm_prediction == 1 :
         result = "News is fake"
     else:
-        result = "News is true"  
-    
-    """for i in range(len(lstm_prediction)):
-        if lstm_prediction[i].item() >= 0.5:
-            result.append(1)
-        else:
-            result.append(0)"""
-    """if pred >= 0.5:
-        result.append(1)
-    else:
-        result.append(0)"""   
-    return render_template('index.html', text_data=result)
+        result = "News is true"   
+    return render_template('result.html', text_data=result)
 
 @app.route('/services')
 def services():
